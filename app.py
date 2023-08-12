6e1d66a1eeb5f8bc4f7d3b5a569b80299d5d8b2b
satish <sateesh@gobillion.co>
2023-08-12 17:06:17 +0530

    fix every things

--- app.py
+++ app.py
@@ -5,15 +5,13 @@ import docx
 import csv
 from io import StringIO
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from oldtemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
-
-
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -37,21 +35,7 @@ def get_file_text(files):
                 text += para.text + "\n"
         elif file_extension == 'txt':
             text += file.getvalue().decode("utf-8") + "\n"
-        elif file_extension == 'csv':
-            csv_text = get_csv_text(file)
-            text += csv_text + "\n" 
     return text
-
-def get_csv_text(file):
-    text = ""
-    
-    file_contents = StringIO(file)
-    csv_reader = csv.reader(file_contents, delimiter=',')
-    for row in csv_reader:
-        Email = row
-        text += ', '.join(row) + "\n"    
-    return text
-
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
@@ -63,18 +47,13 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-
-def get_vectorstore(text_chunks):
+def get_vectorstore(text_chunks):   
     embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
-
-def get_conversation_chain(vectorstore):
+def get_conversation_chain(vectorstore):   
     llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
@@ -84,56 +63,46 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
-
-def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
-
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-
-
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with Own Documents",
-                       page_icon=":books:")
-    st.write(css, unsafe_allow_html=True)
+    st.set_page_config(page_title="Chat with Own Documents", page_icon=":books:")
+  
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+        st.session_state.chat_history = []
 
-    st.header("Chat with Own Document :books:")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        handle_userinput(user_question)
-
+    # Display chat history here
+    for message in st.session_state.chat_history:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    
+    if prompt := st.chat_input("Ask a question about your documents:"):
+        with st.chat_message("user"):
+            st.markdown(prompt)
+            st.session_state.chat_history.append({"role": "user", "content": prompt})
+        
+        with st.chat_message("assistant"):
+            if st.session_state.conversation:
+                response = st.session_state.conversation({'question': prompt})
+                full_response = response['answer']  # Extract the response text
+                st.markdown(full_response)
+                st.session_state.chat_history.append({"role": "assistant", "content": full_response})
+            else:
+                st.error("Please process documents first.")
+            
     with st.sidebar:
         st.subheader("Your documents")
         uploaded_files = st.file_uploader(
             "Upload your PDF,Txt,Doc here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
-                # get pdf text
-                #raw_text = get_pdf_text(uploaded_files)
                 raw_text = get_file_text(uploaded_files)
-                #raw_text = get_file_text_new(uploaded_files)
-                # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-
-                # create vector store
                 vectorstore = get_vectorstore(text_chunks)
-
-                # create conversation chain
                 st.session_state.conversation = get_conversation_chain(
                     vectorstore)
-
 
 if __name__ == '__main__':
     main()
--- htmlTemplates.py
+++ htmlTemplates.py
@@ -1,51 +1,17 @@
 bot_template = '''
 <div class="chat-message bot">
-    <div class="avatar">
-        <img src="https://gobillion-images-production.s3.ap-south-1.amazonaws.com/icons/customerSupport.jpg">
-    </div>
     <div class="message">{{MSG}}</div>
 </div>
 '''
 
 user_template = '''
 <div class="chat-message user">
-    <div class="avatar">
-        <img src="https://gobillion-images-production.s3.ap-south-1.amazonaws.com/question.png">
-    </div>
     <div class="message">{{MSG}}</div>
 </div>
 '''
 
 css = '''
 <style>
-.chat-container {
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    overflow: hidden;
-}
-.stApp > div[data-testid="stBlock"] {
-            flex: 1;
-        }
-.chat-messages {
-    flex: 1;
-    overflow-y: auto;
-    padding: 1rem;
-}
-
-.user-input-container {
-    padding: 1rem;
-    background-color: #fff;
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    z-index: 999;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
 .chat-message {
     padding: 1rem;
     border-radius: 0.5rem;
@@ -59,7 +25,7 @@ css = '''
     color: #000;
     justify-content: flex-end;
     align-self: flex-end;
-    margin-left: auto;
+    margin-left: auto; /* This will push the user message to the right */
 }
 
 .chat-message.bot {
@@ -67,7 +33,7 @@ css = '''
     color: #000;
     justify-content: flex-start;
     align-self: flex-start;
-    margin-right: auto;
+    margin-right: auto; /* This will push the bot message to the left */
 }
 
 .chat-message .avatar {
@@ -75,53 +41,20 @@ css = '''
 }
 
 .chat-message .avatar img {
-    max-width: 60px;
-    max-height: 60px;
-    border-radius: 50%;
-    object-fit: cover;
-}
-
-.chat-message .message {
-    width: 80%;
-    padding: 0 1rem;
-    border-radius: 0.5rem;
-}
-
-.user-input-container .avatar {
-    width: 20%;
-}
-
-.user-input-container .avatar img {
     max-width: 40px;
     max-height: 40px;
     border-radius: 50%;
     object-fit: cover;
 }
-
-.user-input-container input[type="text"] {
-    flex: 1;
-    padding: 0.5rem;
-    border-radius: 0.5rem;
-    border: 1px solid #ccc;
-    margin-left: 1rem;
+.stTextInput {
+    margin-top: auto;
+    transform: translateY(55vh);
+    display: inline-block;
 }
-.footer {
-            position: fixed;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            padding: 10px;
-            background-color: #f0f0f0;
-            text-align: center;
-        }
-
-.user-input-container button {
-    padding: 0.5rem 1rem;
-    border: none;
-    border-radius: 0.5rem;
-    background-color: #007bff;
-    color: #fff;
-    cursor: pointer;
+.chat-message .message {
+    width: 80%;
+    padding: 0 1rem;
+    border-radius: 0.5rem; /* Rounded corners for the message */
 }
 </style>
 '''
